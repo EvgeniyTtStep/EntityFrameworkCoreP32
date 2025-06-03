5f773cacacbe15d436d5264de1a975f70ed52a56
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-06-03 19:36:24 +0300

    Lesson5-6

--- EntityFrameworkCoreP32/Program.cs
+++ EntityFrameworkCoreP32/Program.cs
@@ -10,29 +10,41 @@ class Program
         //CREATE 
 
 
-        context.Engines.Add(new Engine() { Name = "Gaz 1.6" });
-        context.Engines.Add(new Engine() { Name = "Gaz 2.5" });
-        context.SaveChanges();
+        // context.Engines.Add(new Engine() { Name = "Gaz 1.6" });
+        // context.Engines.Add(new Engine() { Name = "Gaz 2.5" });
+        // context.SaveChanges();
+        //
+        //
+        // context.Toyotas.Add(new Toyota()
+        //     { Name = "Prius", ReleaseDate = DateTime.Now, Engine = context.Engines.Find(1) });
+        // context.Toyotas.Add(new Toyota() { Name = "Supraaa", Engine = context.Engines.Find(2) });
+        // context.Toyotas.Add(new Toyota() { Name = "Selica" });
+        // context.Toyotas.Add(new Toyota() { Name = "Land Cruiser 200" });
+        //
+        // context.SaveChanges();
+        //
+        // context.Toyotas.Update(new Toyota() { Name = context.Toyotas.Find(4).Name = "Land Cruiser 777" });
+        //
+        // // context.Remove(context.Toyotas);
+        //
+        // context.SaveChanges();
 
+
+        // ========== Lesson5-6 =================
+        context.Drivers.Add(new Driver() { Name = "Driver Max" });
+        context.Drivers.Add(new Driver() { Name = "Driver Bob" });
+        context.SaveChanges();
 
         context.Toyotas.Add(new Toyota()
-            { Name = "Prius", ReleaseDate = DateTime.Now, Engine = context.Engines.Find(1) });
-        context.Toyotas.Add(new Toyota() { Name = "Supraaa", Engine = context.Engines.Find(2) });
-        context.Toyotas.Add(new Toyota() { Name = "Selica" });
-        context.Toyotas.Add(new Toyota() { Name = "Land Cruiser 200" });
+            { Name = "Sequoia", Drivers = new List<Driver> { new Driver() { Name = "Driver Zak" } } });
+        
+        context.Toyotas.Add(new Toyota() { Name = "Tundra", Drivers = new List<Driver>
+        {
+            context.Drivers.Find(1)!, 
+            context.Drivers.Find(2)!
+        }});
 
-        context.SaveChanges();
-
-        context.Toyotas.Update(new Toyota() { Name = context.Toyotas.Find(4).Name = "Land Cruiser 777" });
-
-        // context.Remove(context.Toyotas);
-
-        context.SaveChanges();
-        
-        
-        
-        
-        
+       context.SaveChanges();
 
         //READ
         foreach (var toyota in context.Toyotas)
